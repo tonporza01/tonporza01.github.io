3ff3678344b5cde4c9c591e9ebb41672d1bc357f
aroowpee <tonporza01@gmail.com>
2024-02-18 13:55:07 +0700

    edit home page 85% wait build footer and CSS

--- app.js
+++ app.js
@@ -13,6 +13,27 @@ closeShopping.addEventListener('click', ()=>{
     body.classList.remove('active');
 })
 
+// // This element Carousel
+// const buttons = document.querySelectorAll('[data-carousel-button]');
+
+// buttons.forEach(button => {
+    
+//     button.addEventListener('click', ()=>{
+//         const offset = button.dataset.carouselButton === 'next' ? 1 : -1
+//         const slides = button
+//         .closest('[data-carousel]')
+//         .querySelector('[data-slides]')
+        
+//         const activeSlide = slides.querySelector('[data-active]')
+//         let newindex = [...slides.children].indexOf(activeSlide) + offset
+//         if(newindex < 0) newindex = slides.children.length - 1
+//         if(newindex >= slides.children.length) newindex = 0
+        
+//         slides.children[newindex].dataset.active = true
+//         delete activeSlide.dataset.active
+//     })
+// })
+
 let products = [
     {
         id: 1,
