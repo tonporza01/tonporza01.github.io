779046df096e457b7014fed268a24ae4887696c7
aroowpee <tonporza01@gmail.com>
2024-02-21 19:53:49 +0700

    optimize space

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 // This element Carousel
-
-// const buttons = document.querySelectorAll('[data-carousel-button]');
-
-// buttons.forEach(button => {
-    
-//     button.addEventListener('click', ()=>{
-//         const offset = button.dataset.carouselButton === 'next' ? 1 : -1
-//         const slides = button
-//         .closest('[data-carousel]')
-//         .querySelector('[data-slides]')
-        
-//         const activeSlide = slides.querySelector('[data-active]')
-//         let newindex = [...slides.children].indexOf(activeSlide) + offset
-//         if(newindex < 0) newindex = slides.children.length - 1
-//         if(newindex >= slides.children.length) newindex = 0
-        
-//         slides.children[newindex].dataset.active = true
-//         delete activeSlide.dataset.active
-//     })
-// })
-
 const buttons = document.querySelectorAll('[data-carousel-button]');
 
 buttons.forEach(button => {
